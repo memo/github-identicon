ea1fbdc31a9f90cf976cfa39d243266d5c8059d4
riko <rwopiiw@gmail.com>
2025-09-10 13:32:18 -0400

    types again

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,8 @@ declare module "github-identicon" {
     /**
      * generate identicon
      * @param value input to hash
-     * @param size image size (default 420)
-     * @returns png buffer of the identicon
+     * @param size image size (default: 420)
+     * @returns png buffer
     */
     export function identicon(value: string | number, size?: number): Buffer;
 
